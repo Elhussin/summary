8fc63322d21ff9f3bdfc21872b5cd5f80a62f88f
Elhussin <hasin3112@gmail.com>
2024-08-08 00:55:00 +0300

    updat media path

--- summary/models.py
+++ summary/models.py
@@ -45,6 +45,7 @@ class Course(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='course_images/', blank=True, null=True)
     updated_at = models.DateTimeField(auto_now=True)
     # timestamp=m
 
--- summayies_app/settings.py
+++ summayies_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'summary.User'
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'summary/media')
