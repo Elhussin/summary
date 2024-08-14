38849014993a3077ee1f95585f7524b615fb123b
Elhussin <hasin3112@gmail.com>
2024-08-14 08:51:43 +0300

    update Profilr user view and add ches own courses

--- summary/static/summary/js/profile.js
+++ summary/static/summary/js/profile.js
@@ -1,45 +1,40 @@
-
 import {
-    getCourses,
-    getCourse,
-    addCourse,
-    updateCourse,
-    deleteCourse,
-  } from "./api.js";
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} from "./api.js";
+//   boxes
 const coursContainer = document.getElementById("cours-container");
+const coressAdd = document.getElementById("coress-add");
+// buttons
+const coressViewBtn = document.getElementById("course-view-btn");
+const coressAddBtn = document.getElementById("course-add-btn");
 
-//  add listner event to view our romve form display 
-const coressAddBtn=document.getElementById("course-add-btn");
-coressAddBtn.addEventListener('click',()=>{
-    const coressAdd=document.getElementById("coress-add")
-    coressAdd.innerHTML=`
-        <h3>New Course</h3>
-        <form enctype="multipart/form-data" method="post" id="course_form" >
-       
-        <div class="form-group">
-            <label for="user_id">Title</label>
-            <input class="form-control" type="text" name="name" id="name">
-        </div>
-        <div class="form-group">
-            <label for="description">Description</label>
-            <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
-        </div>
-        <div class="mb-3">
-            <label for="id_image" class="form-label">Image:</label>
-            <input class="form-control" type="file" name="image" id="id_image"  accept="image/*">
-        </div>
-        <input type="submit" id="send" class="btn btn-primary float-right m-3" value="Add Course"/>
-    </form>
-    `
-    if(coressAdd.style.display=='none'){
-        coressAdd.style.display='block'
-    }else{
-        coressAdd.style.display='none'
-    }
-})
+// document.getElementById("nav-view").onclick = function() {navIteam()};
+document.addEventListener("DOMContentLoaded", (event) => {
+  //  add listner event to view our romve form display
+  // display block our none for add course itream
+coressAddBtn.onclick = ()=> { displayIteam(coressAdd, coursContainer, "block");};
+
+  //  add event for courses view button
+  coressViewBtn.addEventListener("click", () => {
+    displayIteam(coursContainer, coressAdd, "flex");
+    fetchCourses();
+  });
+});
+
+
+// function to display our none diplay iteams 
+const displayIteam = (iteamView, IteamOff, diplayType) => {
+  if (iteamView.style.display == "none") {
+    iteamView.style.display = diplayType;
+    IteamOff.style.display = "none";
+  } else {
+    iteamView.style.display = "none";
+  }
+};
 
 
 
@@ -47,27 +42,55 @@ coressAddBtn.addEventListener('click',()=>{
 const fetchCourses = async () => {
   try {
     const courses = await getCourses();
-    renderCardsUser(courses)
+    renderCardsUser(courses);
   } catch (error) {
     console.error("Error:", error);
   }
 };
 
-//  add event for courses view button 
-const coressViewBtn=document.getElementById("course-view-btn");
-coressViewBtn.addEventListener('click',()=>{
-    console.log("clike")
-    fetchCourses()
-})
 
-//  render course dat to view in course view 
+
+
+//  Add New Course
+
+document.getElementById("course_form")
+  .addEventListener("submit", function (event) {
+    var message =''
+    // block out Send form
+    event.preventDefault();
+    // get form detiels
+    let formData = new FormData(this);
+    // Send data by Axios
+    axios
+      .post("/api/courses/", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "X-CSRFToken": formData.get("csrfmiddlewaretoken"), // get from form  CSRF Token
+        },
+      })
+      .then((response) => {
+        message = "Course added successfully";
+        alertMessage(message);
+        document.getElementById("course_form").reset();
+        console.log("Course added successfully:", response.data);
+      })
+      .catch((error) => {
+        message = "Error adding course";
+        alertMessage(message );
+        console.error("Error adding course:", error);
+      });
+  });
+
+
+
+//  render course dat to view in course view
 const renderCardsUser = (data) => {
-    const userId=document.getElementById('user_id').value;
-    const  filteredData= data.filter(item => item.user_id == userId);
-    console.log("render",userId,filteredData)
-    coursContainer.innerHTML += filteredData
-      .map(
-        (item) => `
+  const userId = document.getElementById("user_id").value;
+  const filteredData = data.filter((item) => item.user_id == userId);
+  console.log("render", userId, filteredData);
+  coursContainer.innerHTML += filteredData
+    .map(
+      (item) => `
           <div class="card" id="${item.id}" >
                     <img src="${item.image}" alt="${item.name}">
                     <div class="card-content">
@@ -75,44 +98,16 @@ const renderCardsUser = (data) => {
                         <p class="card-description">${item.description}</p>
                     </div>
           </div>`
-      ).join("");
-}
+    )
+    .join("");
+};
 
-
-
-
-//  Add New Course  
-document.getElementById('course_form').addEventListener('submit', function(event) {
-    // block out Send form
-    event.preventDefault(); 
-    // get form detiels
-    let formData = new FormData(this);
-    // Send data by Axios
-    axios.post('/api/courses/', formData, {
-        headers: {
-            'Content-Type': 'multipart/form-data',
-            'X-CSRFToken': formData.get('csrfmiddlewaretoken') // get from form  CSRF Token
-        }
-    })
-    .then(response => {
-        let message= "Course added successfully"
-        alert(message)
-        document.getElementById('course_form').reset();
-        console.log('Course added successfully:', response.data);
-    })
-    .catch(error => {
-        message= "Error adding course:"
-        alert(message)
-        console.error('Error adding course:', error);
-    });
-});
-
-})
-
-// declear alert meesage 
-function alert(message){
-    const messageAlrt=document.getElementById('message-alrt')
-    messageAlrt.style.display='block'
-    messageAlrt.innerHTML=`<p>`+message+`</p>`
-    setInterval(function(){messageAlrt.style.display='none'},100*100);
+// declear alert meesage
+function alertMessage(message) {
+  const messageAlrt = document.getElementById("message-alrt");
+  messageAlrt.style.display = "block";
+  messageAlrt.innerHTML = `<p>` + message + `</p>`;
+  setInterval(function () {
+    messageAlrt.style.display = "none";
+  }, 100 * 100);
 }
